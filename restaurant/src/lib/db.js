469de8e1ea4276dd2e620f4642b8b243b3beaec5
KeerthanaaBhat <kbneria@gmail.com>
2025-04-24 13:17:29 +0530

    Bluemoon Restaurant

--- restaurant/src/lib/db.js
+++ restaurant/src/lib/db.js
@@ -2,7 +2,9 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  
+  ssl: {
+    rejectUnauthorized: false, // Required for NeonDB
+  },
 });
 
 // Optional test connection (use only once if you want to verify)
