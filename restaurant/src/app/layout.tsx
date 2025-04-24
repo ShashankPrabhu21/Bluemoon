3117343b6dfe0877bb9214971d5690e972586041
KeerthanaaBhat <kbneria@gmail.com>
2025-04-24 18:55:03 +0530

    Bluemoon Restaurant

--- restaurant/src/app/layout.tsx
+++ restaurant/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google"; // Geist_Mono is NOT available
+import { Inter } from "next/font/google"; 
 import "./globals.css";
 
 import Footer from "./components/footer";
