4415f359ce8d9600aa60546638378defbc9fa07a
KeerthanaaBhat <kbneria@gmail.com>
2025-04-25 08:06:43 +0530

    Bluemoon Restaurant

--- restaurant/src/app/api/subscribe/route.js
+++ restaurant/src/app/api/subscribe/route.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Ensure this is set in your .env file
+  connectionString: process.env.DATABASE_URL, 
 
 });
 
