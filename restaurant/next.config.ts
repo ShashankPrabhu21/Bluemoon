a8b4401d03fcc79e67008a96c5109a8e3a4a70dc
KeerthanaaBhat <kbneria@gmail.com>
2025-05-02 14:53:07 +0530

    Bluemoon Restaurant

--- restaurant/next.config.ts
+++ restaurant/next.config.ts
@@ -1,20 +1,25 @@
-import type { NextConfig } from "next";
+//next.config.js
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
   },
+  basePath: '', // You can leave this blank if not using a subpath
+  assetPrefix: '', // You can leave this blank unless using CDN
+  headers: async () => [
+    {
+      source: "/_next/static/(.*)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=0, must-revalidate",
+        },
+      ],
+    },
+  ],
 };
-
-module.exports = {
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-  },
-};
-
 
 module.exports = nextConfig;
 
-export default nextConfig;
